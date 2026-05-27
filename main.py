a6730231a4f480100f459affb0cbde795a72de7a
hydralgorithm <hydralgorithm@gmail.com>
2026-05-28 01:56:23 +0530

    POST http method testing and implementation

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None 
 
 @app.get("/")
 def root():
@@ -9,3 +18,14 @@ def root():
 @app.get("/posts")
 def get_posts():
     return{"data": "These are your posts"}
+
+@app.post("/createposts")
+# def create_posts(payLoad: dict = Body(...)):
+#     print(payLoad)
+#     return {"new_post":f"Title: {payLoad['title']}  Content: {payLoad['content']}"}
+# title str, content str
+
+def create_posts(new_post: Post):
+    print(new_post)
+    print(new_post.content)
+    return {"data":"new post"}
